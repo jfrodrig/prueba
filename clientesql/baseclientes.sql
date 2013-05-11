c263f3de53b4db929251231a504c690f1c63d328
jhon <jfrodrig@misena.edu.co>
2013-05-11 02:35:41 -0500

    exportacion base de datos my sql

---
+++ clientesql/baseclientes.sql
@@ -0,0 +1,88 @@
+﻿# Host: localhost  (Version: 5.6.11)
+# Date: 2013-05-11 02:26:09
+# Generator: MySQL-Front 5.3  (Build 2.77)
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+USE `baseclientes`;
+
+#
+# Source for table "client1s"
+#
+
+DROP TABLE IF EXISTS `client1s`;
+CREATE TABLE `client1s` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `birth` date DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "client1s"
+#
+
+INSERT INTO `client1s` VALUES (1,'jhon rodriguez','2008-03-10','masculino','ibague','ibague','','2013-05-11 04:54:37','2013-05-11 04:57:32'),(2,'raquel castiblanco','2011-05-11','femenino','ibague','ibague','','2013-05-11 04:58:02','2013-05-11 04:58:02'),(3,'carlos','2011-05-11','hombre','ibague','ibague','','2013-05-11 06:35:45','2013-05-11 06:37:30');
+
+#
+# Source for table "clients"
+#
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `birth` date DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `departament` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "clients"
+#
+
+
+#
+# Source for table "schema_migrations"
+#
+
+DROP TABLE IF EXISTS `schema_migrations`;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "schema_migrations"
+#
+
+INSERT INTO `schema_migrations` VALUES ('20130510172644'),('20130510184518'),('20130510202832');
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
